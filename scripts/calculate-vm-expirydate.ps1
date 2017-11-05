7a875466e85866ea73d9dc10d75c5155df69ce42
Stefan Johner <sjohner@posteo.ch>
2017-11-05 20:14:33 +0100

    Removed Output

--- scripts/calculate-vm-expirydate.ps1
+++ scripts/calculate-vm-expirydate.ps1
@@ -3,11 +3,9 @@ Param(
 	[Parameter(Mandatory=$True)][int]$vmLifetimeDays
 )
 
-# Calculate VM expiry date
-Write-Host $env:vmLifetime
+# Calculate VM expiration date based on given lifetime
 $today = Get-Date -Hour 0 -Minute 00 -Second 00
 $expirydate = $today.AddDays($vmLifetimeDays)
 
 #Set VSTS release variable
 Write-Host "##vso[task.setvariable variable=vmExpirationDate]$expirydate"
-Write-Host $env:vmExpirationDate
