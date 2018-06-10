dbe56d012a771594dfaf20e69a4df47eb4e1e537
Stefan Johner <sjohner@posteo.ch>
2018-06-10 11:05:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Microsoft System Center Service Manager Builds Database E2EVC Amsterdam</title>
+        <title>Microsoft System Center Service Manager Builds Database E2EVC 2018 Amsterdam</title>
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
