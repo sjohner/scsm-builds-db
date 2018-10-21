a6c3d00421a528526a07a5998f0a1f4a93ab973b
Stefan Johner <sjohner@posteo.ch>
2018-10-21 20:08:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Microsoft SCSM Builds Database</title>
+        <title>Microsoft SCSM Builds Database v0.1</title>
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
